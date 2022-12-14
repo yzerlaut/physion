7e5aeb8a602a570880a11152f3631295e32f3544
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-12-14 12:13:33 +0100

    random dots ok

--- src/physion/visual_stim/stimuli/random_dots.py
+++ src/physion/visual_stim/stimuli/random_dots.py
@@ -1,55 +1,45 @@
-"""
-==========================================
- ---  template for new visual stimuli ---
-==========================================
-
-copy this and rename to the desired script name
-
-/!\ need to add the new script to the "stimuli/__init__.py" 
-"""
 import numpy as np
 
 from physion.visual_stim.main import vis_stim_image_built,\
         init_times_frames, init_bg_image
 
-##########################################
-##  ----    STIMULUS TEMPLATE    --- #####
-##########################################
+####################################
+##  ----    RANDOM DOTS    --- #####
+####################################
 
 params = {"movie_refresh_freq":2,
           # default param values:
           "presentation-duration":3,
           "size (deg)":4.,
-          "radius (deg)":40.,
           "ndots (#)":7,
+          "seed (#)":1,
           "dotcolor (lum.)":-1,
-          "bg-color (lum.)":0.5,
-          # now we set the range of possible values:
-          "size-1": 0.01, "size-2": 100, "N-size": 0,
-          "radius-1": 0.001, "radius-2": 100, "N-radius": 0,
-          "ndots-1": 1, "ndots-2": 1000, "N-ndots": 0,
-          "bg-color-1": 0., "bg-color-2": 1., "N-bg-color": 0,
-          "dotcolor-1": -1, "dotcolor-2": 1, "N-dotcolor": 0}
+          "bg-color (lum.)":0.5}
     
+def compute_new_image_with_dots(cls, index,
+                                away_from_edges_factor=4):
+
+    dot_size_pix = int(np.round(cls.angle_to_pix(cls.experiment['size'][index]),0))
+    Nx = int(cls.x.shape[0]/dot_size_pix)
+    Nz = int(cls.x.shape[1]/dot_size_pix)
+
+    img = init_bg_image(cls, index)
+    for n in range(cls.experiment['ndots'][index]):
+        ix, iz = (np.random.choice(np.arange(away_from_edges_factor, Nx-away_from_edges_factor)[::2],1, replace=False)[0],
+                np.random.choice(np.arange(away_from_edges_factor,Nz-away_from_edges_factor)[::2],1, replace=False)[0])
+        img[dot_size_pix*ix:dot_size_pix*(ix+1),
+            dot_size_pix*iz:dot_size_pix*(iz+1)] = cls.experiment['dotcolor'][index]
+    return img
 
 class stim(vis_stim_image_built):
     """
-    stimulus specific visual stimulation object
-
-    all functions should accept a "parent" argument that can be the 
-    multiprotocol holding this protocol
     """
 
     def __init__(self, protocol):
 
         super().__init__(protocol,
-                         keys=['radius', 'bg-color', 'ndots',
-                               'size', 'dotcolor', 'seed'])
+                         keys=['bg-color', 'ndots', 'size', 'dotcolor', 'seed'])
 
-        ## /!\ inside here always use self.refresh_freq 
-        ##        not the parent cls.refresh_freq 
-        # when the parent multiprotocol will have ~10Hz refresh rate,
-        ##                this can remain 2-3Hz
         self.refresh_freq = protocol['movie_refresh_freq']
 
 
@@ -60,41 +50,20 @@ class stim(vis_stim_image_built):
         return the frame at a given time point
         """
         cls = (parent if parent is not None else self)
+        return compute_new_image_with_dots(cls, index)
 
-        img = init_bg_image(cls, index)
+    def get_frames_sequence(self, index, parent=None):
+        """
+        get frame seq
+        """
 
-        # do you image construction/processing here:
-        for i in range(int(cls.experiment['ndots'][index])):
+        cls = (parent if parent is not None else self)
 
-            pos = np.random.randn(2)*cls.experiment['radius'][index]
+        time_indices, times, FRAMES = init_times_frames(cls, index, self.refresh_freq)
 
-            self.add_dot(img, pos,
-                         cls.experiment['size'][index],
-                         cls.experiment['dotcolor'][index],
-                         type='circle')
+        np.random.seed(int(cls.experiment['seed'][index]+3*index)) # changing seed at each realization
+        for iframe, t in enumerate(times):
+            img = compute_new_image_with_dots(cls, index)
+            FRAMES.append(self.image_to_frame(img))
 
-        return img
-
-
-    ### HERE YOU CAN OVERWRITE THE DEFAULT plot_stim_picture FUNCTION
-
-    # def plot_stim_picture(self, episode, ax,
-                          # parent=None, 
-                          # label={'degree':20,
-                                 # 'shift_factor':0.02,
-                                 # 'lw':1, 'fontsize':10},
-                          # vse=False,
-                          # arrow={'length':20,
-                                 # 'width_factor':0.05,
-                                 # 'color':'red'}):
-
-        # """
-        # """
-        # cls = (parent if parent is not None else self)
-
-        # tcenter = .5*(cls.experiment['time_stop'][episode]-\
-                      # cls.experiment['time_start'][episode])
-        
-        # ax = self.show_frame(episode, tcenter, ax=ax,
-                             # parent=parent,
-                             # label=label)
+        return time_indices, FRAMES, self.refresh_freq
