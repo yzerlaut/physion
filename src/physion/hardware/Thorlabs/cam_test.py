565c9b889ebb55c053f10fe8f3ca5560385fc141
yann <yann.zerlaut@icm-institute.org>
2023-06-27 14:19:27 +0100

    adding thorlab cam test

---
+++ src/physion/hardware/Thorlabs/cam_test.py
@@ -0,0 +1,29 @@
+from thorcam.camera import ThorCam
+
+class MyThorCam(ThorCam):
+    def received_camera_response(self, msg, value):
+        super(MyThorCam, self).received_camera_response(msg, value)
+        if msg == 'image':
+            return
+        print('Received "{}" with value "{}"'.format(msg, value))
+    def got_image(self, image, count, queued_count, t):
+        print('Received image "{}" with time "{}" and counts "{}", "{}"'
+              .format(image, t, count, queued_count))
+
+cam = MyThorCam()
+
+cam.start_cam_process()
+
+cam.refresh_cameras()
+
+cam.open_camera('03756')
+cam.exposure_range
+cam.play_camera()
+
+cam.stop_playing_camera()
+
+# close the camera
+cam.close_camera()
+
+# close the server and everything
+cam.stop_cam_process(join=True)
