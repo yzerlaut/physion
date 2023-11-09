69ac2c39f86b95faeebb8d515971aff5b81262b9
yann <yann.zerlaut@icm-institute.org>
2023-11-09 11:46:59 +0100

    move env to install

--- src/physion/hardware/FLIRcamera/test.py
+++ src/physion/hardware/FLIRcamera/test.py
@@ -1,4 +1,5 @@
 import matplotlib.pylab as plt
+from PIL import Image
 
 import simple_pyspin, time, sys, os
 import numpy as np
@@ -13,4 +14,7 @@ img = cam.get_array().astype(np.uint8)
 
 plt.imshow(img)
 plt.show()
+
+Image.fromarray(img).save(os.path.join(os.path.expanduser('~'), 'Desktop', 'test.png'))
+print('test image saved on the desktop')
 
