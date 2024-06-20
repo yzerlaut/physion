6f2d65c08b3d7a0251685df69876d65ae4eec90e
Yann Zerlaut <yann.zerlaut@gmail.com>
2024-06-20 15:43:15 +0200

    for pv-sst-dendrites submission

--- src/physion/__main__.py
+++ src/physion/__main__.py
@@ -4,7 +4,6 @@ from PyQt5 import QtWidgets
 from physion.gui.main import MainWindow
 app = QtWidgets.QApplication(sys.argv)
 
-
 try:
     import qdarktheme # pip install pyqtdarktheme
     app.setStyleSheet(qdarktheme.load_stylesheet('dark'))
