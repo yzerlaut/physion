6c4a761cb52827118049225f1540e9e34de90ef4
Yann Zerlaut <yann.zerlaut@gmail.com>
2024-08-23 10:15:19 +0200

    update opencv draft

--- src/physion/visual_stim/openCV_test.py
+++ src/physion/visual_stim/openCV_test.py
@@ -8,21 +8,26 @@ if '.mp4' in sys.argv[-1]:
     movie = cv2.VideoCapture(sys.argv[-1])
 
     cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-    cv2.moveWindow('frame', 0, 982)
-    # cv2.moveWindow('frame', 500, -300)
-    # cv2.resizeWindow('frame', 2100, 1700)
+    cv2.moveWindow('frame', 0, -700)
     cv2.resizeWindow('frame', 1280, 720)
 
-    # cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     # Check if camera opened successfully
     if (movie.isOpened()== False): 
       print("Error opening video stream or file")
-     
+    
+    times = []
     # Read until video is completed
     while(movie.isOpened()):
+
+      tic = time.time()
       # Capture frame-by-frame
+      #movie.set(2, 0.)
       ret, frame = movie.read()
+
+      times.append(time.time()-tic)
+
       if ret == True:
      
         # Display the resulting frame
@@ -35,12 +40,15 @@ if '.mp4' in sys.argv[-1]:
       # Break the loop
       else: 
         break
+
      
     # When everything done, release the video capture object
     movie.release()
      
     # Closes all the frames
     cv2.destroyAllWindows()
+
+    print(1e3*np.mean(times))
 else:
     print('need to provide a mp4 as argument !')
 
