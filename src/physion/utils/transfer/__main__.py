aa80d3745194c5cb20e0264f11ba2539521c1e69
Yann Zerlaut <yann.zerlaut@gmail.com>
2024-10-18 11:28:18 +0200

    update types of transfer

--- src/physion/utils/transfer/__main__.py
+++ src/physion/utils/transfer/__main__.py
@@ -1,21 +1,32 @@
-from .tools import TYPES
-
 import argparse
+import shutil, os, glob
+
+from .types import TYPES
+
+Help = ''
+for t in TYPES:
+    Help += ' \n - %s ' % t
+
 
 
 parser=argparse.ArgumentParser()
 parser.add_argument("source", type=str)
 parser.add_argument("destination", type=str)
 parser.add_argument("type", type=str, 
-                    help="should be one of :"+str(TYPES.keys()))
+                    help="should be one of %s :" % Help)
 args = parser.parse_args()
 
 
 if args.type in TYPES:
-    shutil.copytree(args.source,
-                    args.destination,
-                    ignore=TYPES[args.type])
+    
+    for f in [f for f in os.listdir(args.source) if not f.startswith('.')]:
+
+        print(' - copying "%s" [...] ' % os.path.join(args.source, f))
+        shutil.copytree(os.path.join(args.source, f),
+                        os.path.join(args.destination, f),
+                        dirs_exist_ok=True,
+                        ignore=TYPES[args.type])
+
 else:
     print(' need to choose a key from types:')
-    for k in TYPES:
-        print(' - %s' % k)
+    print(Help)
--- src/physion/utils/transfer/types.py
+++ src/physion/utils/transfer/types.py
@@ -1,13 +1,20 @@
-import sys, os, pathlib, shutil
-
+"""
+script to build the different ignore function to be used in shutil.copytree
+    for COPYING SPECIFIC SUBSETS OF DATA
+"""
+import shutil
 
 def ignore_all_behav_image_folders(Dir, f):
     return ('FaceCamera' in Dir) or ('RigCamera' in Dir)
 
-def ignore_to_take_only_processed_imaging(Dir, f):
-    return (not 'TSeries' in Dir) or\
-                        ((not '.npy' in f) and\
-                        (not '.xml' in f))  
+def ignore_to_take_only_processed_imaging(Dir, files):
+    print(Dir)
+    print(files)
+    # return [not( ('TSeries' in Dir) and\
+                   # (('.npy' in f) or ('.xml' in f)) )\
+                        # for f in files]
+    # return [not ('TSeries' in Dir) for f in files]
+    return 
 
 def ignore_to_take_only_processed_imaging_with_vids(Dir, f):
     return (not 'TSeries' in Dir) or \
@@ -46,15 +53,25 @@ def ignore_to_take_only_xml(Dir, f):
 
 
 TYPES = {
-    'processed-Imaging':ignore_to_take_only_processed_imaging,
-    'processed-Imaging-wVids':ignore_to_take_only_processed_imaging_with_vids,
-    'processed-Imaging-wBinary':ignore_to_take_only_processed_imaging_with_binary,
-    'raw-Imaging-only':ignore_to_take_only_raw_imaging,
-    'processed-Behavior':ignore_to_take_only_processed_behavior,
-    'stim.+behav. (processed)':ignore_all_behav_image_folders,
-    'nwb':ignore_to_take_only_NWBs,
-    'npy':ignore_to_take_only_npy,
-    'xml':ignore_to_take_only_xml,
-    'Imaging (+binary)':{},
+    'processed-Imaging':shutil.ignore_patterns('*.ome.tif', 'Reference*', 
+                                               '*.avi', '*.mp4', '*.wmv',
+                                               '*.env', '*.bin'),
+    'processed-Imaging-wVids':shutil.ignore_patterns('*.ome.tif', 'Reference*', 
+                                                     '*.env', '*.bin'),
+    'processed-Imaging-wBinary':shutil.ignore_patterns('*.ome.tif', 'Reference*', 
+                                                       '*.env'),
+    'raw-Imaging-only':shutil.ignore_patterns('*.npy', 'suite2*', '*.env'),
+    'video-Imaging-only':shutil.ignore_patterns('*.ome.tif', 
+                                                'db.npy', 'ops.npy', 
+                                                'suite2*', '*.env'),
+    'processed-Behavior':shutil.ignore_patterns('FaceCamera-*', 
+                                                'RigCamera-*',
+                                                'TSerie*'),
+    'nwb':shutil.ignore_patterns('*.npy', '*.env', '*.tif', '*.bin', 
+                                    '*.mp4', '*.wmv', '*.avi', '*.xml'),
+    'npy':shutil.ignore_patterns('*.nwb', '*.env', '*.tif', '*.bin', 
+                                    '*.mp4', '*.wmv', '*.avi', '*.xml'),
+    'xml':shutil.ignore_patterns('*.nwb', '*.env', '*.tif', '*.bin', 
+                                    '*.mp4', '*.wmv', '*.avi', '*.npy'),
     'all':None,
 }
