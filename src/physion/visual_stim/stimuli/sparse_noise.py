c629e8a886c09df58532b43b42119e9d6d526d13
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-01-21 13:10:57 +0100

    adding sparse noise

--- src/physion/visual_stim/stimuli/sparse_noise.py
+++ src/physion/visual_stim/stimuli/sparse_noise.py
@@ -21,48 +21,21 @@ import numpy as np
 from physion.visual_stim.main import visual_stim, init_bg_image
 
 ####################################
-##  ----    RANDOM DOTS    --- #####
+##  ----    SPARSE NOISE   --- #####
 ####################################
 
 params = {"movie_refresh_freq":30.,
-          "presentation-duration":20*60.,
-          "refresh (Hz)":2.,
-          "size (deg)":5.,
-          "ndots (#)":7,
+          "presentation-duration":0.5,
+          "presentation-interstim-period":0,
+          "screen-width (deg)":116,
+          "screen-height (deg)":84,
+          "size (deg)":6.,
+          "sparseness (%)":4,
+          "black-white-ratio (%)":50,
           "seed (#)":1,
-          "contrast (lum.)":1.,
-          "contrast (lum.)":1.,
+          # "shape ":0, # 0 is square, 1 is circle
           "bg-color (lum.)":0.5}
     
-def compute_new_image_with_dots(cls, index,
-                                seed=0,
-                                away_from_edges_factor=3):
-
-    np.random.seed(seed)
-    dot_size= int(cls.experiment['size'][index])
-    Nx = int(cls.x.max()/dot_size)
-    Nz = int(cls.z.max()/dot_size)
-
-    # dot center positions
-    xx, zz = np.meshgrid(np.arange(-Nx+away_from_edges_factor, 
-                                   Nx+1-away_from_edges_factor)[::2],
-                         np.arange(-Nz+away_from_edges_factor, 
-                                   Nz+1-away_from_edges_factor)[::2],
-                         indexing='ij')
-
-    ii = np.random.choice(np.arange(len(xx.flatten())),
-                         int(cls.experiment['ndots'][index]), replace=False)
-    X = xx.flatten()[ii]
-    Z = zz.flatten()[ii]
-
-    img = init_bg_image(cls, index)
-    for x, z in zip(X, Z):
-
-        cls.add_dot(img, (x*dot_size, z*dot_size),
-                    cls.experiment['size'][index],
-                    cls.experiment['dotcolor'][index])
-
-    return img
 
 class stim(visual_stim):
     """
@@ -71,12 +44,42 @@ class stim(visual_stim):
     def __init__(self, protocol):
 
         super().__init__(protocol,
-                         keys=['bg-color', 
-                               'ndots', 
-                               'size', 
-                               'refresh', 
-                               'dotcolor', 
-                               'seed'])
+                         keys=['bg-color', 'size'])
+
+        # screen size 
+        Dx = protocol["screen-width (deg)"]
+        Dy = protocol["screen-height (deg)"]
+
+        # calculate number of dots:
+        self.ndots = int(\
+                np.floor(((Dx*Dy) * protocol['sparseness (%)']/100. /\
+                        np.max(self.experiment['size'])**2 ) / 2.) * 2.) 
+        iThresh = int(protocol['black-white-ratio (%)']/100.*self.ndots)
+
+        # print(iThresh, self.ndots)
+
+        np.random.seed(int(protocol['seed (#)']))
+
+        index = 0
+        for i in range(self.ndots):
+            self.experiment['dot-x-%i' % i] = \
+                        np.random.uniform(self.x.min()+\
+                            self.experiment['size'][index]/2.,
+                                          self.x.max()-\
+                            self.experiment['size'][index]/2.,
+                            len(self.experiment['index']))
+            self.experiment['dot-z-%i' % i] = \
+                        np.random.uniform(self.z.min()+\
+                            self.experiment['size'][index]/2.,
+                                          self.z.max()-\
+                            self.experiment['size'][index]/2.,
+                            len(self.experiment['index']))
+            if i>=iThresh:
+                self.experiment['dot-color-%i' % i] = 0.*\
+                        np.ones(len(self.experiment['index']))
+            else:
+                self.experiment['dot-color-%i' % i] = 1.*\
+                        np.ones(len(self.experiment['index']))
 
 
     def get_image(self, index,
@@ -85,25 +88,38 @@ class stim(visual_stim):
         """ 
         return the frame at a given time point
         """
-        new_seed = (1+self.experiment['seed'][index])**2+\
-            int(time_from_episode_start*self.experiment['refresh'][index])
+        img = init_bg_image(self, index)
 
-        return compute_new_image_with_dots(self, index, seed=int(new_seed))
+        for i in range(self.ndots):
+
+            self.add_dot(img, 
+                        (self.experiment['dot-x-%i' % i][index],
+                         self.experiment['dot-z-%i' % i][index]),
+                         self.experiment['size'][index],
+                         self.experiment['dot-color-%i' % i][index])
+
+        return img
+
+def insure_no_overlap_with():
+    pass
 
 if __name__=='__main__':
 
     from physion.visual_stim.build import get_default_params
 
     params = get_default_params('sparse-noise')
+    params['N-repeat'] = 10
 
     import time
     import cv2 as cv
 
     Stim = stim(params)
 
-    t0 = time.time()
-    while True:
+    t0, index = time.time(), 0
+    while True and index<(len(Stim.experiment['index'])-1):
+        index = int((time.time()-t0)\
+                /Stim.protocol['presentation-duration'])
         cv.imshow("Video Output", 
-                  Stim.get_image(0, time_from_episode_start=time.time()-t0).T)
+                  Stim.get_image(index).T)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
