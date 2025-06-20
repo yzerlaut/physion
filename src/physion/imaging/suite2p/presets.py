2bd324e698abe5fafc7197e4a7ba0a67428acfe7
yann <yann.zerlautr@proton.me>
2025-06-20 11:57:30 +0200

    working on suite2p configs

--- src/physion/imaging/suite2p/presets.py
+++ src/physion/imaging/suite2p/presets.py
@@ -8,18 +8,19 @@ presets = {\
                            # registration
                            "nonrigid": False,
                            # functional detection
-                           "sparse_mode":0,
+                           "sparse_mode":False,
                            "allow_overlap":True,
                            "denoise":0,
                            "connected":True,
                            "threshold_scaling":0.5,
-                           "cell_diameter":20,
+                           "high_pass":300,
                            "spatial_hp_detect":25,
+                           "cell_diameter":10,
                            # neuropil
                            "inner_neuropil_radius": 2,
-                           "min_neuropil_pixels": 100, 
+                           "min_neuropil_pixels": 200, 
                            "flow_threshold": 0.1,
-                           "cellprob_threshold": -2.0},\
+                           "cellprob_threshold": 0.5},\
         "SomasOnly-zoomed":{\
                            # registration
                            "nonrigid": False,
