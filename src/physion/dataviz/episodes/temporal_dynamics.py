df7e1aa72c838f4c12e10c8566b0888521fd2897
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-11-04 09:21:01 +0100

    up

--- src/physion/dataviz/episodes/temporal_dynamics.py
+++ src/physion/dataviz/episodes/temporal_dynamics.py
@@ -6,6 +6,7 @@ import physion.utils.plot_tools as pt
 def plot_response_dynamics(keys,
                             path=os.path.expanduser('~'),
                             average_by='sessions',
+                            significantly_responsive=True,
                             colors=None,
                             with_label=True,
                             fig_args={}):
@@ -25,17 +26,27 @@ def plot_response_dynamics(keys,
             Responses = \
                     np.load(os.path.join(path, 'Deconvolved_%s.npy' % key), 
                             allow_pickle=True)
-
+            
             if average_by=='sessions':
                 # mean significant responses per session
-                Deconvolved = [np.mean(Response['Deconvolved'][Response['significant'],:],
-                                axis=0) for Response in Responses]
+                if significantly_responsive:
+                    Deconvolved = [np.mean(Response['Deconvolved'][Response['significant'],:],
+                                    axis=0) for Response in Responses]
+                else:
+                    Deconvolved = [np.mean(Response['Deconvolved'],
+                                            axis=0) for Response in Responses]
+
 
             elif average_by=='ROIs':
                 # mean significant responses per session
-                Deconvolved = np.concatenate([\
-                                Response['Deconvolved'][Response['significant'],:]\
+                if significantly_responsive:
+                    Deconvolved = np.concatenate([\
+                                    Response['Deconvolved'][Response['significant'],:]\
                                                             for Response in Responses])
+                else:
+                    Deconvolved = np.concatenate([\
+                                    Response['Deconvolved'] for Response in Responses])
+
             elif average_by=='subjects':
                 subjects = np.array([Response['subject']\
                                         for Response in Responses])
@@ -43,22 +54,31 @@ def plot_response_dynamics(keys,
                 for subj in np.unique(subjects):
                     sCond = (subjects==subj)
                     # concatenate all ROIs for a given subject, then mean
-                    Deconvolved.append(\
-                        np.mean(\
-                            np.concatenate(\
-                                [Responses[i]['Deconvolved'][Responses[i]['significant'],:]\
-                                    for i in np.arange(len(subjects))[sCond]]),
-                            axis=0))
+                    if significantly_responsive:
+                        Deconvolved.append(\
+                            np.mean(\
+                                np.concatenate(\
+                                    [Responses[i]['Deconvolved'][Responses[i]['significant'],:]\
+                                        for i in np.arange(len(subjects))[sCond]]),
+                                axis=0))
+                    else:
+                        Deconvolved.append(\
+                            np.mean(\
+                                np.concatenate(\
+                                    [Responses[i]['Deconvolved']\
+                                        for i in np.arange(len(subjects))[sCond]]),
+                                axis=0))
                     
             else:
                 print("""
                     average_by either: ROIs, sessions, subjects
                         """)
 
-            pt.plot(Responses[0]['t'], 
-                        np.mean(Deconvolved, axis=0), 
-                            sy=stats.sem(Deconvolved, axis=0), 
-                            color=color, ax=ax, ms=2)
+            if len(Responses)>0:
+                pt.plot(Responses[0]['t'], 
+                            np.mean(Deconvolved, axis=0), 
+                                sy=stats.sem(Deconvolved, axis=0), 
+                                color=color, ax=ax, ms=2)
 
             if with_label:
 
