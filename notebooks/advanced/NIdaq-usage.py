1815c54dc82ef7e923a70d2ea07f8d579127cbf1
yann <yann.zerlaut@proton,me>
2025-11-20 16:54:40 +0100

    working NIdaq usage notebook

--- notebooks/advanced/NIdaq-usage.py
+++ notebooks/advanced/NIdaq-usage.py
@@ -1,4 +1,11 @@
 # %%
+import sys, time
+sys.path += ['../../src']
+import physion
+
+
+
+# %%
 import numpy as np
 import matplotlib.pylab as plt
 tstop = 0.5
@@ -14,12 +21,31 @@ nT = int(T/dt)
 
 cond = x % (3*nT) < nT
 
-A = np.zeros_like(x)
-A[cond] = 1
+output = np.zeros_like(x)
+output[cond] = 1
 
-plt.plot(t, A)
+plt.plot(t, output)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.grid(True)
-plt.show()
+
+# %%
+acq = physion.hardware.NIdaq.main.Acquisition(\
+                 sampling_rate=fs,
+                 Nchannel_analog_in=2,
+                 outputs=np.array([output], dtype=np.float64),
+                 max_time=tstop)
+
+# %%
+acq.launch()
+tic = time.time()
+while (time.time()-tic)<tstop:
+    pass
+acq.close()
+# %%
+plt.plot(t, acq.analog_data[0][1:])
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+
 # %%
