221ff6bf119f7536cfddbd9ef8a5da7e0cd5b435
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-11-21 15:05:58 +0100

    working-movie-conversion-to-binary

--- notebooks/Build-Movie.py
+++ notebooks/Build-Movie.py
@@ -169,12 +169,16 @@ for time in [3.5, 4., 5., 6]:
 # %%
 from physion.utils import plot_tools as pt
 t0 = 4.5
+array = []
 for t0 in np.linspace(3., 6., 12):
     i0 = np.argmin((rigCamera.times-t0)**2)
-    print(i0, rigCamera.times[i0])
+    array.append(i0)
+    hasFrame, img = rigCamera.cap.read()
     fig, ax = pt.figure(ax_scale=(3,3))
+    i1 = faceCamera.cap.get(cv.CAP_PROP_POS_FRAMES)
+    print(i0, i1, rigCamera.times[i0])
     pt.matrix(
-        np.rot90(rigCamera.get(i0).T, k=3),
+        np.rot90(img[:,:,0].T, k=2),
         vmin=0,
         vmax=255,
         colormap='gray',
@@ -189,9 +193,10 @@ for t0 in np.linspace(3., 6., 12):
     rigCamera.cap.set(cv.CAP_PROP_POS_MSEC, t0*1000)
     fig, ax = pt.figure(ax_scale=(3,3))
     hasFrame, img = rigCamera.cap.read()
+    i0 = faceCamera.cap.get(cv.CAP_PROP_POS_FRAMES)
+    print(i0)
     if hasFrame:
         pt.matrix(
-            # rigCamera.cap.read()[1][:,:,0].T,
             np.rot90(img[:,:,0].T, k=2),
             vmin=0,
             vmax=255,
@@ -200,4 +205,39 @@ for t0 in np.linspace(3., 6., 12):
         )
         ax.axis('off')
         ax.set_title('t=%.1f' % t0)
+# %%
+import imageio, tempfile
+FILE = os.path.join(raw_data_folder, 'RigCamera.wmv')
+temp = tempfile.mktemp(suffix='.jpg')
+for i in array:
+    fig, ax = pt.figure(ax_scale=(3,3))
+    print(i)
+    img = imageio.imread(FILE, index=i) #, plugin='pyav')
+    pt.matrix(
+        # rigCamera.cap.read()[1][:,:,0].T,
+        np.rot90(img[:,:,0].T, k=2),
+        vmin=0,
+        vmax=255,
+        colormap='gray',
+        ax=ax,
+    )
+    
+
+
+# %%
+import ffmpeg, tempfile
+FILE = os.path.join(raw_data_folder, 'RigCamera.wmv')
+temp = tempfile.mktemp(suffix='.jpg')
+for i in array:
+    (
+        ffmpeg
+        .input(FILE, ss=30)
+        .filter('scale', width, -1)
+        .output(temp, vframes=1)
+        .run()
+    )
+
+
+# %%
+print(i)
 # %%
