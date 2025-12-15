6c28817e9d8bdb6e75284163d38d58fee4723052
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-12-15 11:45:17 +0100

    add-back-centered-flickering-bar

--- src/physion/intrinsic/build_protocols.py
+++ src/physion/intrinsic/build_protocols.py
@@ -14,8 +14,10 @@ def build_json(direction='Up',
                screen='Mouse-Goggles',
                bg_color=0.2,
                period=12,
+               center=0,
                size=7,
-               flicker_size=5):
+               flicker_size=5,
+               length=200):
 
 
     return """
@@ -31,11 +33,9 @@ def build_json(direction='Up',
     "presentation-poststim-period": 0,
     "presentation-interstim-period": 0,
     "N-repeat": 1,
-    "z-min": -40.0,
-    "z-max": 60.0,
-    "x-min": -120.0,
-    "x-max": 20.0,
     "direction": %(direction)s,
+    "bar-center": %(center)s,
+    "bar-length": %(length)s,
     "bar-size": %(size)s,
     "bg-color": %(bg_color)s,
     "flicker-size": %(flicker_size)s,
@@ -46,6 +46,8 @@ def build_json(direction='Up',
        'bg_color':bg_color,
        'size':size,
        'flicker_size':flicker_size,
+       'center':center,
+       'length':length,
        'screen':screen}
 
 if __name__=='__main__':
@@ -66,14 +68,12 @@ if __name__=='__main__':
                             - Dell-2020
                             - Mouse-Goggles
                             - Lilliput
-                            - LN-VR-3screens
                         """)
     parser.add_argument("--bg_color", type=float, default=0.2)
     
     args = parser.parse_args()
 
     folder = os.path.join('physion', 'acquisition', 'protocols', 'movies', args.protocol)
-    #folder = './'
 
     if args.protocol=='intrinsic':
         for period in [6, 12]:
@@ -90,25 +90,11 @@ if __name__=='__main__':
                                        period=period, 
                                        bg_color=args.bg_color))
                 # build the movie
-                os.system('python -m physion.visual_stim.build temp.json')
-                if args.screen=='LN-U3screens':
-                    for i in range(1,4):
-                        os.rename(os.path.join('movies', 'temp', 'movie-%i.wmv' % i),
-                                os.path.join(folder,
-                                            'flickering-bars-period%is' % period,
-                                            '%s-%i.wmv' % (direction,i)))
-                else:
-                    os.rename(os.path.join('movies', 'temp', 'movie.wmv'),
-                            os.path.join(folder,
-                                        'flickering-bars-period%is' % period,
-                                        '%s.wmv' % direction))
-                try:
-                    os.rename('temp.json', 
-                                os.path.join(folder,
-                                            'flickering-bars-period%is' % period,
-                                            'protocol.json'))
-                except FileExistsError:
-                    pass
+                os.system('python -m physion.visual_stim.build temp.json --wmv')
+                os.rename(os.path.join('movies', 'temp', 'movie.wmv'),
+                          os.path.join(folder,
+                                       'flickering-bars-period%is' % period,
+                                       '%s.wmv' % direction))
 
     elif args.protocol=='ocular-dominance':
         for period in [6, 12]:
@@ -130,7 +116,7 @@ if __name__=='__main__':
                                            flicker_size=2,
                                            bg_color=args.bg_color))
                     # build the movie
-                    os.system('python -m physion.visual_stim.build temp.json')
+                    os.system('python -m physion.visual_stim.build temp.json --wmv')
                     os.rename(os.path.join('movies', 'temp', 'movie.wmv'),
                           os.path.join(folder,
                                        'flickering-bars-period%is' % period,
