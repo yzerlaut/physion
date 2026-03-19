b242d4e92410cd54aba78e8f96502ac2665498ed
yann.zerlaut <yann.zerlaut@icm-institute.org>
2026-03-19 19:08:05 +0100

    reworked ephys alignement

---
+++ notebooks/advanced/Ephys-Analysis.py
@@ -0,0 +1,100 @@
+
+# %% [markdown]
+#
+# # Analysis of Neuropixels data
+#
+
+# %%
+import sys, time
+sys.path += [os.path.expanduser('~/physion/src'), '../../src']
+import json
+import numpy as np
+import pandas as pd
+
+from open_ephys.analysis import Session as OpenEphysSession
+from physion.assembling.dataset import read_spreadsheet
+
+import physion.utils.plot_tools as pt
+pt.set_style('dark')
+
+datafolder = os.path.expanduser('~/DATA/2026_03_19').replace('/', os.path.sep)
+
+# datatable, _, analysis = read_spreadsheet(\
+#                         os.path.join(datafolder, 'DataTable0.xlsx'),
+#                                    get_metadata_from='files')
+
+# INTERPROTOCOL_WINDOW = 10. # 
+# PROBE_NAME = 'ProbeA'
+# EXP = 1 # 
+# NODE=0
+
+class Data:
+
+    def __init__(self, datafolder, iRec):
+
+        datatable, _, _ = read_spreadsheet(\
+                                os.path.join(datafolder, 'DataTable0.xlsx'),
+                                        get_metadata_from='files')
+
+        nidaq = np.load(os.path.join(datafolder, datatable['time'][iRec], 'Nidaq.npy'),
+                        allow_pickle=True).item()
+        self.t_nidaq = np.arange(0, len(nidaq['digital'][0]))*nidaq['dt']
+        self.visStim = nidaq['digital'][3]
+
+        nStart, nStop = datatable['nStart'][iRec], datatable['nStop'][iRec]
+        self.t_probe = np.linspace(0, self.t_nidaq[-1], nStop-nStart)
+
+        # load the open-ephys data:
+        session = OpenEphysSession(\
+                        os.path.join(datafolder, datatable['Npx-Folder'][iRec]))
+
+        node = int(datatable['Npx-Rec'][iRec].split('node')[1].split('/')[0])
+        rec_id = int(datatable['Npx-Rec'][iRec].split('rec')[1])-1
+        rec = session.recordnodes[node].recordings[rec_id]
+
+        self.LFP = rec.continuous['ProbeA'].samples[nStart:nStop,:]
+
+        # @Sally
+        # self.spikes = ...
+
+# %%
+data = Data(datafolder, 0)
+
+t0, length = 0, 60
+fig, AX = pt.figure(axes_extents=[[[1,3]],[[1,1]]], ax_scale=(3,1))
+
+SHIFT = 1000 # 1mV between each channel
+cond = (data.t_probe>t0) & (data.t_probe<(t0+length))
+
+for chan in range(10):
+    lfp = data.LFP[cond,chan]
+    lfp = lfp-lfp.mean()
+    AX[0].plot(data.t_probe[cond], lfp+chan*SHIFT, lw=0.5, color=pt.plt.cm.tab20(chan))
+pt.set_plot(AX[0], ['bottom'], ylabel='LFP')
+pt.draw_bar_scales(AX[0], Xbar=1e-3, Ybar=2000, Ybar_label='2mv')
+
+cond = (data.t_nidaq>t0) & (data.t_nidaq<(t0+length))
+AX[1].plot(data.t_nidaq[cond], data.visStim[cond])
+pt.set_plot(AX[1], ['bottom'], xlabel='time (s)', ylabel='vis. stim.\n onset')
+
+# %%
+from scipy.ndimage import gaussian_filter1d
+events = data.t_nidaq[np.flatnonzero(data.visStim[1:]>data.visStim[:-1])]
+
+lfp_events = []
+window = [-1,2] # temporal window
+for e in events:
+    cond = (data.t_probe>(e+window[0])) & (data.t_probe<(e+window[1]))
+    # lfp = gaussian_filter1d(data.LFP[cond,:].mean(axis=-1), 500)
+    lfp = gaussian_filter1d(data.LFP[cond,0], 500)
+    pre = (data.t_probe[cond]>(e+window[0])) & (data.t_probe[cond]<e)
+    lfp_events.append(lfp-lfp[pre].mean())
+minLength = min([len(l) for l in lfp_events])
+lfp_events = [l[:minLength] for l in lfp_events]
+t = data.t_probe[:minLength]+window[0]
+
+fig, ax = pt.figure(ax_scale=(2,3))
+pt.plot(t, 1e-3*np.mean(lfp_events, axis=0), sy=1e-3*np.std(lfp_events, axis=0), ax=ax)
+pt.set_plot(ax, xlabel='time from stim. (s)', ylabel='LFP (mV)')
+
+# %%
---
+++ src/physion/ephys/alignement.py
@@ -0,0 +1,175 @@
+import os
+import numpy as np
+import json
+from scipy.interpolate import interp1d
+
+from physion.acquisition.tools import find_line_props
+
+def load_nidaq_synch_signal(folder):
+    """ 
+    """
+
+    with open(os.path.join(folder, 'metadata.json')) as f:
+        metadata = json.load(f)
+
+    NIdaq = np.load(os.path.join(folder, 'NIdaq.npy'),
+                    allow_pickle=True).item()
+
+    props = find_line_props(
+                metadata['NIdaq']['digital-outputs']['line-labels'])
+
+    ephysSynch_signal = NIdaq['digital'][props['chan']]
+
+    t = np.arange(len(ephysSynch_signal))*NIdaq['dt']
+
+    pulse_onsets = t[:-1][np.flatnonzero(ephysSynch_signal[1:]>ephysSynch_signal[:-1])]
+
+    return t, ephysSynch_signal, pulse_onsets
+
+def build_ttl_from_events(State, Sample):
+    # we start at 0
+    SN, TTL = [Sample[0]-30000], [0]
+    # loop over events
+    for state, sample in zip(State, Sample):
+        if state==1:
+            SN.append(sample); TTL.append(0)
+            SN.append(sample); TTL.append(1)
+        if state==0:
+            SN.append(sample); TTL.append(1)
+            SN.append(sample); TTL.append(0)
+    # we force ending at 0
+    SN.append(sample); TTL.append(0)
+    SN.append(sample+30000); TTL.append(0)
+    return np.array(SN, dtype=np.int32), np.array(TTL, dtype=np.uint8)
+
+def load_OpenEphys(rec,
+                   PROBE_NAME='ProbeA'):
+
+    # find TTL events on Probe A
+    cond = (rec.events['stream_name']==PROBE_NAME) &\
+                (rec.events['sample_number']>0)
+
+    # load the events
+    State = np.array(rec.events['state'][cond])
+    Sample = np.array(rec.events['sample_number'][cond])
+    pulse_onsets = Sample[State==1]
+
+    # build the time array from the set of events
+    SN, TTL = build_ttl_from_events(State, Sample)
+
+    return pulse_onsets, SN, TTL 
+
+
+def find_sampling_match(t, nidaq_Onsets, ephys_Onsets,
+                        Nshift=4, verbose=False):
+    """
+    we find the sample numbers that match the limits of the NIdaq acquisition,
+    then, the samples in [nStart, nStop]
+        have the time sampling:
+            np.linspace(t[0], t[-1], nStop-nStart)
+
+    where t is the nidaq time sampling array
+
+    Because some TTL events can appear without being triggered by the NIdaq
+    we test different shifts to find the right alignement and we take the best !
+        --> to be checked visually in the figure !
+    """
+
+    # varying the shift and computing correlations
+    CC, nMax = [], len(nidaq_Onsets)-int(2*Nshift)
+    for i in range(2*Nshift):
+        CC.append(np.corrcoef(nidaq_Onsets[Nshift:-Nshift], ephys_Onsets[i:nMax+i])[0,1])
+    # finding the best correlation between times:
+    i = int(np.argmax(CC))
+    if verbose:
+        print('best shift found for, i=', i-Nshift)
+    nidaq_onsets, ephys_onsets = nidaq_Onsets[Nshift:-Nshift], ephys_Onsets[i:nMax+i]
+
+    N0 = ephys_onsets[0]
+    t0 = nidaq_onsets[0]
+
+    nMax = np.min([len(nidaq_onsets), len(ephys_onsets)])-2
+
+    dN = ephys_onsets[-1]-N0
+    dT = nidaq_onsets[-1]-t0
+
+    if False:
+        # IN CASE YOU WANT TO MAKE A MINIMIZATION FUNCTION, BUT FOR NOW not necessary...
+        def to_minimize(x):
+            T = (sn-x[0])*x[1]+t0
+            func = interp1d(T, ttl) 
+            probe_signal = func(np.clip(t, T.min(), T.max()))
+            return np.mean((probe_signal-nidaqTTL)**2)
+
+        res = least_squares(to_minimize, [N0, F0],
+                            # max_nfev=10000, method='dogbox',
+                            # ftol=None, xtol=None, verbose=True,
+                            bounds=[(N0-3000, 0.99*F0), (N0+3000, 1.01*F0)])
+        N0, F0 = res.x
+    else:
+        F0 = dT/dN
+
+    nStart = N0-int(t0/F0)
+    nStop = N0+dN+int((t[-1]-dT-t0)/F0) # we add dN to limit precision loss
+
+    return nStart, nStop
+
+
+def sampling_match(iRec,
+                   session,
+                   datafolder,
+                   DF,
+                   with_fig=False,
+                   verbose=False):
+
+    t, ephysSynch_signal, nidaq_onsets = load_nidaq_synch_signal(
+                                os.path.join(datafolder, DF['time'][iRec]))
+    
+    # reload the open-ephys data:
+    node = int(DF['Npx-Rec'][iRec].split('node')[1].split('/')[0])
+    rec_id = int(DF['Npx-Rec'][iRec].split('rec')[1])-1
+    rec = session.recordnodes[node].recordings[rec_id]
+    # prepared ---> load
+    pulse_onsets, SN, TTL = load_OpenEphys(rec)
+
+    # restrict to previously identified range:
+    irange=np.arange(DF['i0'][iRec], np.min([DF['i1'][iRec],len(SN)]))
+    pulse_cond = (pulse_onsets>=SN[irange[0]]) & (pulse_onsets<=SN[irange[-1]])
+
+    # find the matching sample range
+    nStart, nStop = find_sampling_match(t, nidaq_onsets, pulse_onsets[pulse_cond],
+                                        verbose=verbose)
+
+    # we now match the time sampling in the data
+    cond = (SN>=nStart) & (SN<=nStop)
+    T = (SN[cond]-nStart)*(t[-1]-t[0])/(nStop-nStart)
+    func = interp1d(T, TTL[cond], 
+                    bounds_error=False,
+                    fill_value=0)
+    # we build a probe signal from the interpolation of the data
+    probe_signal = func(t)
+
+    width = 2.5
+    if with_fig:
+        import physion.utils.plot_tools as pt
+        fig, AX = pt.figure(axes=(4,2), ax_scale=(1.6,.7), top=1.5, hspace=1.6, wspace=0.3)
+        fig.suptitle('protocol #%i (%i episodes)' % (iRec+1, np.sum(pulse_cond)))
+
+        for i, t0 in enumerate([0.5, t[-1]/2+1, 3.*t[-1]/4., t[-1]]):
+
+            pt.annotate(AX[0][i], 't=%.1fs' % t0, (0.1,1))
+
+            # nidaq
+            cond = (t>(t0-width)) & (t<(t0+width))
+            AX[0][i].plot(t[cond][::10], ephysSynch_signal[cond][::10])
+            pt.set_plot(AX[0][i], xlabel='NIdaq time (s)', ylabel='TTL\n(from NIdaq)' if i==0 else None)
+
+            # open-ephys
+            AX[1][i].plot(t[cond][::10], probe_signal[cond][::10])
+            pt.set_plot(AX[1][i], xlabel='$F \\cdot (N- N_0) $ time (s)', ylabel='TTL\n(on Probe)' if i==0 else None)
+
+            pt.set_common_xlims([AX[0][i], AX[1][i]])
+
+        return nStart, nStop, fig, pulse_onsets[pulse_cond], nidaq_onsets
+    else:
+        return nStart, nStop
