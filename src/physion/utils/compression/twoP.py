825a81dc0dc3f952fb84b52e960f22a539f03755
Yann Zerlaut <yann.zerlaut@gmail.com>
2026-08-27 14:40:32 +0200

    working-2P-to-binary-conversion

--- src/physion/utils/compression/twoP.py
+++ src/physion/utils/compression/twoP.py
@@ -21,6 +21,24 @@ from physion.imaging.bruker.xml_parser import bruker_xml_parser
 from physion.utils.progressBar import printProgressBar
 from physion.utils.paths import FOLDERS
 
+from pynwb import NWBHDF5IO, NWBFile
+from dateutil.tz import tzlocal
+from datetime import datetime
+from uuid import uuid4
+from hdmf.data_utils import DataChunkIterator
+
+from pynwb.ophys import (
+    CorrectedImageStack,
+    Fluorescence,
+    ImageSegmentation,
+    MotionCorrection,
+    OnePhotonSeries,
+    OpticalChannel,
+    RoiResponseSeries,
+    TwoPhotonSeries,
+)
+
+
 def imaging_to_movie_gui(self,
                        tab_id=3):
 
@@ -31,7 +49,7 @@ def imaging_to_movie_gui(self,
     self.cleanup_tab(tab)
 
     self.add_side_widget(tab.layout, 
-            QtWidgets.QLabel(' _-* Conversion to Movie File *-_ '))
+            QtWidgets.QLabel(' _-* Conversion of 2P Imaging *-_ '))
 
     self.add_side_widget(tab.layout, QtWidgets.QLabel("" , self))
 
@@ -56,7 +74,7 @@ def imaging_to_movie_gui(self,
     self.add_side_widget(tab.layout, 
             QtWidgets.QLabel("Compression / Format : ", self))
     self.typeBox = QtWidgets.QComboBox()
-    self.typeBox.addItems(['8bit-LOG-mp4', '16bit-avi (lossless)'])
+    self.typeBox.addItems(['nwb', 'binary', '8bit-LOG-mp4', '16bit-avi (lossless)'])
     self.add_side_widget(tab.layout, self.typeBox)
 
     self.add_side_widget(tab.layout, QtWidgets.QLabel("" , self))
@@ -67,6 +85,199 @@ def imaging_to_movie_gui(self,
     
     self.refresh_tab(tab)
     self.show()
+
+
+def write_nwb(filename, 
+              data=np.zeros((512,512,10), dtype=np.uint16),
+              close_io=True):
+
+    start_time = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
+    nwbfile = NWBFile(
+        session_description="demonstrate iterative write",
+        identifier=str(uuid4()),
+        session_start_time=start_time,
+    )
+
+    device_model = nwbfile.create_device_model(
+        name="Thorlabs Bergamo II Model",
+        description="Two-photon microscope for in vivo imaging",
+        manufacturer="Thorlabs",
+        model_number="Bergamo II",
+    )
+
+    device = nwbfile.create_device(
+        name="Thorlabs Bergamo II",
+        description="Two-photon microscope for in vivo imaging",
+        model=device_model,
+        serial_number="SN-123456789",
+    )
+
+    optical_channel = OpticalChannel(
+        name="OpticalChannel",
+        description="an optical channel",
+        emission_lambda=500.0,
+    )
+
+    imaging_plane = nwbfile.create_imaging_plane(
+        name="ImagingPlane",
+        optical_channel=optical_channel,
+        imaging_rate=30.0,
+        description="a very interesting part of the brain",
+        device=device,
+        excitation_lambda=600.0,
+        indicator="GFP",
+        location="V1",
+        grid_spacing=[0.01, 0.01],
+        grid_spacing_unit="meters",
+        origin_coords=[1.0, 2.0, 3.0],
+        origin_coords_unit="meters",
+    )
+
+    # the image data will be stored inside the NWB file
+    two_p_series = TwoPhotonSeries(
+        name="TwoPhotonSeries",
+        description="Raw 2p data",
+        data=data,
+        imaging_plane=imaging_plane,
+        rate=1.0,
+        unit="normalized amplitude",
+    )
+    nwbfile.add_acquisition(two_p_series)
+
+    # Write the data to file
+    io = NWBHDF5IO(filename, "w")
+    io.write(nwbfile)
+    if close_io:
+        io.close()
+        del io
+        io = None
+    return io
+
+def convert_to_nwb_Movie(TS_folder):
+
+    xml_file = get_files_with_extension(TS_folder, 
+                                        extension='.xml')[0]
+    xml = bruker_xml_parser(xml_file)
+
+    Ly, Lx = int(xml['settings']['linesPerFrame']),\
+                    int(xml['settings']['pixelsPerLine'])
+
+    if not os.path.isdir(os.path.join(TS_folder.replace('TSeries', 'nwb'))):
+        os.mkdir(os.path.join(TS_folder.replace('TSeries', 'nwb')))
+
+    print('\n Analyzing: "%s" ' % TS_folder)
+    for chan in xml['channels']:
+    
+        print('    --> Channel: ', chan)
+        nframes = len(xml[chan]['tifFile'])
+        movie_rate = 1./float(xml['settings']['framePeriod'])
+        FILES = xml[chan]['tifFile']
+
+        DICT = {'compression':'None'}
+        
+        for p in np.unique(xml[chan]['depth_index']):
+
+            plane_cond = (xml[chan]['depth_index']==p)
+
+            vid_name = os.path.join(TS_folder.replace('TSeries', 'nwb'),
+                                     '%s-plane%i.nwb' %\
+                                    (chan.replace(' ','-'), p))
+
+            def data_iter_func(max_chunks=10):
+                i = 0
+                while i<len(FILES[plane_cond]):
+                    img = np.array(Image.open(os.path.join(TS_folder, FILES[plane_cond][i])),
+                                dtype='uint16')
+                    print(img)
+                    i+=1
+                    yield img
+                return
+
+            write_nwb(vid_name,
+                      data = DataChunkIterator(data=data_iter_func,
+                                               dtype=np.dtype('uint16')))
+
+            # print('\n  [...]  Building the binary file: "%s" ' % vid_name)
+            # with open(vid_name, 'wb') as bin:
+
+            #     for i, f in enumerate(FILES[plane_cond]):
+            #         try:
+            #             # load 16-bit image
+            #             img = np.array(Image.open(os.path.join(TS_folder, f)),
+            #                         dtype='uint16')
+            #             # write in movie
+            #             bin.write(img.tobytes())
+            #             if i%100==0:
+            #                 printProgressBar(i, nframes)
+            #             success[i] = True
+            #         except BaseException as be:
+            #             print('problem with frame:', f)
+            #             print(be)
+
+            print(' [ok] "%s" succesfully created !' % vid_name)
+            # DICT['Frames_succesfully_in_movie-plane%i'%p]= success
+
+        np.save(os.path.join(TS_folder.replace('TSeries', 'nwb'), 
+                             '%s-summary.npy'%chan.replace(' ','-')),
+                DICT)
+        print(' [ok] Frames-summary.npy succesfully created !')
+
+def convert_to_binary(TS_folder):
+
+    xml_file = get_files_with_extension(TS_folder, 
+                                        extension='.xml')[0]
+    xml = bruker_xml_parser(xml_file)
+
+    Ly, Lx = int(xml['settings']['linesPerFrame']),\
+                    int(xml['settings']['pixelsPerLine'])
+
+    if not os.path.isdir(os.path.join(TS_folder.replace('TSeries', 'binary'))):
+        os.mkdir(os.path.join(TS_folder.replace('TSeries', 'binary')))
+
+    print('\n Analyzing: "%s" ' % TS_folder)
+    for chan in xml['channels']:
+    
+        print('    --> Channel: ', chan)
+        nframes = len(xml[chan]['tifFile'])
+        movie_rate = 1./float(xml['settings']['framePeriod'])
+        FILES = xml[chan]['tifFile']
+
+        DICT = {'compression':'log+mp4v'}
+        
+        for p in np.unique(xml[chan]['depth_index']):
+
+            vid_name = os.path.join(TS_folder.replace('TSeries', 'binary'),
+                                     '%s-plane%i.bin' %\
+                                    (chan.replace(' ','-'), p))
+
+            plane_cond = (xml[chan]['depth_index']==p)
+            success = np.zeros(len(FILES[plane_cond]), dtype=bool)
+
+            print('\n  [...]  Building the binary file: "%s" ' % vid_name)
+            with open(vid_name, 'wb') as bin:
+
+                for i, f in enumerate(FILES[plane_cond]):
+                    try:
+                        # load 16-bit image
+                        img = np.array(Image.open(os.path.join(TS_folder, f)),
+                                    dtype='uint16')
+                        # write in movie
+                        bin.write(img.tobytes())
+                        if i%100==0:
+                            printProgressBar(i, nframes)
+                        success[i] = True
+                    except BaseException as be:
+                        print('problem with frame:', f)
+                        print(be)
+
+            print(' [ok] "%s" succesfully created !' % vid_name)
+            DICT['Frames_succesfully_in_movie-plane%i'%p]= success
+
+        np.save(os.path.join(TS_folder.replace('TSeries', 'binary'), 
+                             '%s-summary.npy'%chan.replace(' ','-')),
+                DICT)
+        print(' [ok] Frames-summary.npy succesfully created !')
+
 
 def run_imaging_to_movie(self):
 
@@ -79,6 +290,10 @@ def run_imaging_to_movie(self):
             print('      use only from command line')
         elif '8bit-LOG' in self.typeBox.currentText():
             convert_to_log8bit_mp4(f)
+        elif 'binary' in self.typeBox.currentText():
+            convert_to_binary(f)
+        elif 'nwb' in self.typeBox.currentText():
+            convert_to_nwb_Movie(f)
         else:
             print(' compression type not recognized')
         print(f)
@@ -93,6 +308,9 @@ def convert_to_16bit_avi(TS_folder):
                     int(xml['settings']['pixelsPerLine'])
 
     print('\n Analyzing: "%s" ' % TS_folder)
+
+    if not os.path.isdir(os.path.join(TS_folder.replace('TSeries', 'lossless'))):
+        os.mkdir(os.path.join(TS_folder.replace('TSeries', 'lossless')))
 
     for chan in xml['channels']:
    
@@ -122,6 +340,10 @@ def convert_to_log8bit_mp4(TS_folder):
 
     Ly, Lx = int(xml['settings']['linesPerFrame']),\
                     int(xml['settings']['pixelsPerLine'])
+
+
+    if not os.path.isdir(os.path.join(TS_folder.replace('TSeries', 'log8bit'))):
+        os.mkdir(os.path.join(TS_folder.replace('TSeries', 'log8bit')))
 
     print('\n Analyzing: "%s" ' % TS_folder)
     for chan in xml['channels']:
@@ -267,12 +489,13 @@ def create_compressed_folder(folder,
                     ignore=shutil.ignore_patterns('*.ome.tif', #'Reference*', 
                                                   'CYCLE*', '*.bin'))
 
-    if os.path.isdir(\
-            os.path.join(folder.replace('TSeries', key), 'original_suite2p')):
-        shutil.rmtree(os.path.join(folder.replace('TSeries', key), 'original_suite2p'))
+    # if os.path.isdir(\
+    #         os.path.join(folder.replace('TSeries', key), 'original_suite2p')):
+    #     shutil.rmtree(os.path.join(folder.replace('TSeries', key), 'original_suite2p'))
 
-    shutil.move(os.path.join(folder.replace('TSeries', key), 'suite2p'),
-                os.path.join(folder.replace('TSeries', key), 'original_suite2p'))
+    # if os.path.isdir(os.path.join(folder.replace('TSeries', key), 'suite2p')):
+    #     shutil.move(os.path.join(folder.replace('TSeries', key), 'suite2p'),
+    #                 os.path.join(folder.replace('TSeries', key), 'original_suite2p'))
 
 
 def find_TSeries_folders(folder):
@@ -368,8 +591,15 @@ if __name__=='__main__':
             create_compressed_folder(folder, 
                                      key=args.compression)
 
-            if args.compression=='lossless':
+            if args.compression=='nwb':
+                convert_to_nwb_Movie(folder)
+
+            elif args.compression=='binary':
+                convert_to_binary(folder)
+
+            elif args.compression=='lossless':
                 convert_to_16bit_avi(folder)
+
             else:
                 convert_to_log8bit_mp4(folder)
             
