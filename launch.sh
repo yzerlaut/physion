c8d6c84dfe1ed348df0a7cb6e01a7017b691e557
Yann Zerlaut <yann.zerlaut@gmail.com>
2026-09-25 10:12:47 +0200

    up

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 export QT_QPA_PLATFORM=xcb
-cd ~/lab-notebook/yann/physion/src
+cd ~/code/yann/physion/src
 ~/miniforge3/bin/python -m physion 
--- src/physion/utils/misc.py
+++ src/physion/utils/misc.py
@@ -1,4 +1,5 @@
 import tempfile
+import numpy as np
 
 class dummy_datafolder:
     def __init__(self):
@@ -11,3 +12,20 @@ class dummy_parent:
         self.stop_flag = False
         self.datafolder = dummy_datafolder()
 
+def parse_int_list(string):
+    """
+    "9,3,4,"  -> [9, 3, 4]
+    "9-13"    -> [9, 10, 11, 12, 13]
+    "1,3-5,8" -> [1, 3, 4, 5, 8]
+    """
+    values = []
+    for item in string.replace(' ', '').split(','):
+        if item=='':
+            continue
+        if '-' in item[1:]: # range (a leading "-" is a negative sign)
+            i = item.index('-', 1)
+            start, stop = int(item[:i]), int(item[i+1:])
+            values += list(range(start, stop+1))
+        else:
+            values.append(int(item))
+    return np.array(values, dtype=int)
